14e60d4f1b7f8d6dbfea723e9028972a5197f7a2
adambens <adambens@umich.edu>
2018-01-28 21:34:50 -0500

    Completed HW

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -1,6 +1,7 @@
 ## SI 364
 ## Winter 2018
 ## HW 2 - Part 1
+## Adam Benson
 
 ## This homework has 3 parts, all of which should be completed inside this file (and a little bit inside the /templates directory).
 
@@ -15,6 +16,9 @@ from flask import Flask, request, render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
+import json
+import requests
+
 
 #####################
 ##### APP SETUP #####
@@ -27,7 +31,11 @@ app.config['SECRET_KEY'] = 'hardtoguessstring'
 ###### FORMS #######
 ####################
 
-
+#This is Part 2
+class AlbumEntryForm(FlaskForm):
+	albumName = StringField('Enter the name of an album:', validators=[Required()])
+	albumRating = RadioField('How much do you like this album? (1 low, 3 high)', choices=[('1','1'),('2','2'),('3','3')], validators=[Required()])
+	submit = SubmitField('Submit')
 
 
 ####################
@@ -42,6 +50,63 @@ def hello_world():
 @app.route('/user/<name>')
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
+
+
+#Part 1
+
+#Artist Form
+@app.route('/artistform')
+def artist_form():
+    return render_template('artistform.html')
+
+#Artist Links
+@app.route('/artistlinks')
+def artist_links():
+    return render_template('artist_links.html')
+
+#Artist Info
+@app.route('/artistinfo')
+def artist_info():
+	params = dict()
+	baseURL = "https://itunes.apple.com/search?"
+	artist = request.args.get('artist',"")
+	params = {'term': artist, 'entity' : 'musicTrack'}
+	response = requests.get(baseURL, params = params)
+	jsonResponse = json.loads(response.text)
+	artistObject = jsonResponse['results']
+	return render_template('artist_info.html', artist=artist, objects=artistObject)
+
+
+#Specific Artist
+@app.route('/specific/song/<artist_name>', methods = ['GET', 'POST'])
+def specific_artist(artist_name):
+	params = dict()
+	baseURL = "https://itunes.apple.com/search?"
+	params = {'term': artist_name, 'entity' : 'musicTrack'}
+	response = requests.get(baseURL, params = params).json()['results']
+	return render_template('specific_artist.html', results = response)
+
+
+###########################################################################
+## Part 2 #################################################################
+###########################################################################
+
+#Album Entry
+@app.route('/album_entry', methods = ['GET', 'POST'])
+def album_entry():
+    myForm = AlbumEntryForm()
+    return render_template('album_entry.html',form=myForm)
+
+#Album Result
+@app.route('/album_result', methods= ['POST', 'GET'])
+def album_result():
+	name = request.args.get('albumName')
+	rating = request.args.get('albumRating')
+	results = [name, rating]
+	return render_template('album_data.html', results= results)
+
+
+
 
 
 if __name__ == '__main__':
